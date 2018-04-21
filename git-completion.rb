2cabd27e396c7b924b8da361c9004064d00c5533
Kevin Stone <kevinastone@gmail.com>
2018-04-20 20:38:13 -0700

    bumped sha256

--- git-completion.rb
+++ git-completion.rb
@@ -4,7 +4,7 @@ class GitCompletion < Formula
   homepage "https://github.com/git/git"
   version "2.0.3"
   url "https://raw.githubusercontent.com/git/git/v2.0.3/contrib/completion/git-completion.bash"
-  sha1 "23e8678d00c9a5b85e875bac1814b44d5fb46da9"
+  sha256 "2d10c0b62913ef07619b2c275e86eca158533d546027de5d193385fcf027c797"
   head "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
 
   def install
